68b51d0c11fdb99912f17319809f7498729c84d9
Oleg Shishkin <shishkin1966@gmail.com>
2018-10-12 16:56:45 +0300

    Оптимизация

--- app/src/main/java/shishkin/cleanarchitecture/mvi/app/screen/fragment/sidemenu/SideMenuRouter.java
+++ app/src/main/java/shishkin/cleanarchitecture/mvi/app/screen/fragment/sidemenu/SideMenuRouter.java
@@ -1,9 +1,9 @@
 package shishkin.cleanarchitecture.mvi.app.screen.fragment.sidemenu;
 
+import shishkin.cleanarchitecture.mvi.app.SLUtil;
 import shishkin.cleanarchitecture.mvi.app.model.BaseModelRouter;
 import shishkin.cleanarchitecture.mvi.app.screen.fragment.digital_currencies.DigitalCurrenciesFragment;
 import shishkin.cleanarchitecture.mvi.app.screen.fragment.map.MapFragment;
-import shishkin.cleanarchitecture.mvi.app.screen.fragment.scanner.ScannerFragment;
 import shishkin.cleanarchitecture.mvi.app.screen.fragment.setting.SettingFragment;
 import shishkin.cleanarchitecture.mvi.app.screen.fragment.val_curs.ValCursFragment;
 import shishkin.cleanarchitecture.mvi.sl.model.AbsModel;
@@ -35,6 +35,6 @@ public class SideMenuRouter extends BaseModelRouter {
     }
 
     public void showScanner() {
-        showFragment(ScannerFragment.newInstance());
+        SLUtil.getScannerUnion().scan();
     }
 }
