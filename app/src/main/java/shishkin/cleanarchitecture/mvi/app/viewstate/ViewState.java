9fbfa125ebe6c3146519fc8f26ea71e4d1fbe332
Oleg Shishkin <shishkin1966@gmail.com>
2018-11-04 08:25:46 +0300

    Доработка

--- app/src/main/java/shishkin/cleanarchitecture/mvi/app/viewstate/ViewState.java
+++ app/src/main/java/shishkin/cleanarchitecture/mvi/app/viewstate/ViewState.java
@@ -1,4 +1,8 @@
 package shishkin.cleanarchitecture.mvi.app.viewstate;
 
+import shishkin.cleanarchitecture.mvi.sl.Secretary;
+import shishkin.cleanarchitecture.mvi.sl.SecretaryImpl;
+
 public class ViewState {
+    private Secretary<Object> secretary = new SecretaryImpl<>();
 }
